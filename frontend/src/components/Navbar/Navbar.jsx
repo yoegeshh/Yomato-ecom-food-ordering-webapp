1a307fcc7b94f1cef12fb12f9578c2b127bd2a0b
yoegesh <yoegeshkainbox@gmail.com>
2025-09-04 14:46:09 +0530

    setup file structure

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '..//../assets/assets'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className='logo' />
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-App</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="nav-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot">
+            <button>Sign In</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
