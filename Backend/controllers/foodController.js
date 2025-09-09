dabbc6ad13e9ec47114bd68330ed82d9f3f35aca
yoegesh <yoegeshkainbox@gmail.com>
2025-09-09 21:20:42 +0530

    fixed

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -1,3 +1,4 @@
+import { console } from "inspector";
 import foodModel from "../models/foodModel.js";
 import fs from 'fs';
 
@@ -25,4 +26,30 @@ const addFood = async (req,res)=>{
 
 }
 
-export {addFood}
+//all food list
+const listFood = async (req,res)=>{
+    try {
+        const foods = await foodModel.find({});
+        res.json({success:true,data:foods})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+//remove food item
+const removeFood = async (req,res)=>{
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{});//deletes files from uploads folder
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:"Food removed"});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+}
+
+export {addFood,listFood,removeFood}
