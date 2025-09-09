c5082899c19a36538085377f0b90d5a318e3a2b8
yoegesh <yoegeshkainbox@gmail.com>
2025-09-09 21:26:00 +0530

    fixed

--- Backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async ()=>{
-     await mongoose.connect('mongodb+srv://yoegeshkainbox:9852173489@cluster0.uxhwpzj.mongodb.net/yomato').then(()=>console.log("DB Connected"));
-
-}
