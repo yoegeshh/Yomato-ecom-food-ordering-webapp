9eb2c35fcfe4dc707b54bf931325083455e54ee9
yoegesh <yoegeshkainbox@gmail.com>
2025-09-13 20:40:11 +0530

    fetch images from database instead the assests folder

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,6 +1,5 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
-
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props)=>{
@@ -8,6 +7,7 @@ const StoreContextProvider = (props)=>{
     const [cartItems,setCartItems] = useState({});
     const url = "http://localhost:4000"
     const [token,setToken] = useState("");
+    const [food_list,setFoodList] = useState([])
 
     const addToCart = (itemId)=>{
         if(!cartItems[itemId]){
@@ -34,10 +34,19 @@ const StoreContextProvider = (props)=>{
         return totalAmount;
     }
 
+    const fetchFoodList = async ()=>{
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data)
+    }
+
     useEffect(()=>{
-        if (localStorage.getItem("token")) {
+        async function loadData(){
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
             setToken(localStorage.getItem("token"))
         }
+        }
+        loadData();
     },[])
 
     const contextValue = {
